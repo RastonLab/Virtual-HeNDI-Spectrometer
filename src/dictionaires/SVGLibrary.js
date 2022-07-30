774a139e4a0be6ac221cdd860188d552025a7258
VanFossen <nvanfossen@gmail.com>
2022-07-30 16:34:21 -0400

    updated SVGLibrary

--- src/dictionaires/SVGLibrary.js
+++ src/dictionaires/SVGLibrary.js
@@ -1,7 +1,62 @@
-export const imgSource = { 
+export const imgSource = {
+  lecture: "",
+  "turbo-pump": "",
+  "ion-gauge-1": "",
+  "ion-gauge-2": "",
+  "ion-gauge-3": "",
+  "source-chamber": "",
+  "doping-chamber": "",
+  "detection-chamber": "",
+  "diffusion-pump-2": "",
+  "diffusion-pump-1": "",
+  "rotary-pump": "",
+  laser: "",
+};
 
-}
-
-export const toolTips = { 
-
-}
+export const toolTips = {
+  lecture: { title: "Lecture Bottle", text: "" },
+  "turbo-pump": {
+    title: "Turbomolecular Pump",
+    text: 'Evacuates the Detection Chamber by means of an axial compressor which consists of a stack of fans that "push" molecules to the exhaust. The fans rotate at a frequency of ~1 kHz.',
+  },
+  "ion-gauge-1": {
+    title: "Ion Gauge",
+    text: "Pressure of the Detection Chamber, which is similar to that maintained at CERN (close to the vacuum found on the Moons surface).",
+  },
+  "ion-gauge-2": {
+    title: "Ion Gauge",
+    text: "Pressure of the Doping Chamber.",
+  },
+  "ion-gauge-3": {
+    title: "Ion Gauge",
+    text: "Pressure of the Source Chamber. It is relatively high due to the large amount of helium that flows through the nozzle.",
+  },
+  "source-chamber": {
+    title: "Source Chamber",
+    text: "This is where helium nanodroplets are formed.",
+  },
+  "doping-chamber": {
+    title: "Doping Chamber",
+    text: "This chamber is where the analyte molecules condense into the molecular beam.",
+  },
+  "detection-chamber": {
+    title: "Detection Chamber",
+    text: "Houses a mass spectrometer which is used to detect helium nanodroplets.",
+  },
+  "diffusion-pump-1": {
+    title: "Diffusion Pump",
+    text: "This chamber is where the analyte molecules condense into the molecular beam.",
+  },
+  "diffusion-pump-2": {
+    title: "Diffusion Pump",
+    text: "Houses a mass spectrometer which is used to detect helium nanodroplets.",
+  },
+  "rotary-pump": {
+    title: "Rotary Pump",
+    text: "Backs the turbo and diffusion pumps, which require a foreline pressure of <100 mTorr. The exhaust gas is directed to the rooftop.",
+  },
+  laser: {
+    title: "Laser Symbol",
+    text: 'The output from a narrow linewidth quantum cascade laser is overlapped with the helium nanodroplet beam. Before entering the vacuum chamber through the salt window, a small fraction of radiation is reflected to a power meter ("P") and wavemeter ("λ"); the mechanical chopper wheel modulates the laser beam at a frequency of ~100 Hz, which serves as the reference for the lock-in amplifier.',
+  },
+};
